9166c40527612b7b79c6dbf104186165d7adb347
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2025-03-31 13:09:22 +0700

    fix test

--- programs/virtual-curve/src/tests/test_volitility_accumulate.rs
+++ programs/virtual-curve/src/tests/test_volitility_accumulate.rs
@@ -1,56 +1,73 @@
 use crate::{
-    constants::{BASIS_POINT_MAX, BIN_STEP_BPS_U128_DEFAULT, ONE_Q64},
+    constants::{BASIS_POINT_MAX, BIN_STEP_BPS_DEFAULT, BIN_STEP_BPS_U128_DEFAULT, ONE_Q64},
     state::fee::DynamicFeeStruct,
+    tests::price_math::get_price_from_id,
 };
 
 #[test]
 fn test_bin_step_bps_u128() {
-    let result = ONE_Q64.checked_div(BASIS_POINT_MAX.into()).unwrap();
+    let result = ONE_Q64
+        .checked_mul(BIN_STEP_BPS_DEFAULT.into())
+        .unwrap()
+        .checked_div(BASIS_POINT_MAX.into())
+        .unwrap();
     assert_eq!(result, BIN_STEP_BPS_U128_DEFAULT);
 }
 
 #[test]
 fn test_delta_bin_id_basic() {
-    let sqrt_price_a = 1_010_000_000u128; // 101.0
-    let sqrt_price_b = 1_000_000_000u128; // 100.0
-    let result =
-        DynamicFeeStruct::get_delta_bin_id(BIN_STEP_BPS_U128_DEFAULT, sqrt_price_a, sqrt_price_b)
-            .unwrap();
+    let upper_bin_id = 100;
+    let upper_sqrt_price = get_price_from_id(upper_bin_id, BIN_STEP_BPS_DEFAULT).unwrap();
 
-    // Assert exact match
-    // 101.0 / 100 = 1.01 => delta = (1.01 - 1.0) / (1/10_000) = 0.01 / 0.0001 = 100
-    assert_eq!(result, 200);
+    let lower_bin_id = 20;
+    let lower_sqrt_price = get_price_from_id(lower_bin_id, BIN_STEP_BPS_DEFAULT).unwrap();
+
+    let result = DynamicFeeStruct::get_delta_bin_id(
+        BIN_STEP_BPS_U128_DEFAULT,
+        upper_sqrt_price,
+        lower_sqrt_price,
+    )
+    .unwrap();
+
+    let actual_delta_bin = (upper_bin_id - lower_bin_id) * 2;
+    assert_eq!(result, actual_delta_bin as u128);
+}
+
+#[test]
+fn test_delta_bin_id_max_delta() {
+    let delta_bin = 2_000; // 50% price diff
+    let upper_bin_id = 100;
+    let upper_sqrt_price = get_price_from_id(upper_bin_id, BIN_STEP_BPS_DEFAULT).unwrap();
+
+    let lower_bin_id = upper_bin_id - delta_bin;
+    let lower_sqrt_price = get_price_from_id(lower_bin_id, BIN_STEP_BPS_DEFAULT).unwrap();
+
+    let result = DynamicFeeStruct::get_delta_bin_id(
+        BIN_STEP_BPS_U128_DEFAULT,
+        upper_sqrt_price,
+        lower_sqrt_price,
+    )
+    .unwrap();
+
+    let diff = (result)
+        .checked_sub((delta_bin * 2) as u128)
+        .unwrap()
+        .checked_mul(100)
+        .unwrap()
+        .checked_div(result as u128)
+        .unwrap();
+
+    println!("diff {}", diff);
+
+    // less than 10%
+    assert!(diff < 10);
 }
 
 #[test]
 fn test_delta_bin_id_zero_movement() {
     let sqrt_price = 1_000_000_000u128;
-
     let result =
         DynamicFeeStruct::get_delta_bin_id(BIN_STEP_BPS_U128_DEFAULT, sqrt_price, sqrt_price)
             .unwrap();
     assert_eq!(result, 0);
-}
-
-#[test]
-fn test_delta_bin_id_small_movement() {
-    let sqrt_price_a = 1_002_000_000u128;
-    let sqrt_price_b = 1_000_000_000u128;
-
-    let result =
-        DynamicFeeStruct::get_delta_bin_id(BIN_STEP_BPS_U128_DEFAULT, sqrt_price_a, sqrt_price_b)
-            .unwrap();
-    // (1.002 - 1.000) = 0.002 → delta = (1.002 - 1.0) / (1/10_000) = 0.002 / 0.0001 = 20
-    assert_eq!(result, 40)
-}
-
-#[test]
-fn test_get_delta_bin_id_overflow() {
-    //  overflow in price ratio calculation
-    let sqrt_price_a = u128::MAX;
-    let sqrt_price_b = 1u128;
-
-    let result =
-        DynamicFeeStruct::get_delta_bin_id(BIN_STEP_BPS_U128_DEFAULT, sqrt_price_a, sqrt_price_b);
-    assert!(result.is_err());
 }
